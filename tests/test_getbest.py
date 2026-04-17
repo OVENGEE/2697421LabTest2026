af1fef182a9cc66d62c09517aa46363169f94548
morisscofield <msmukwayi@gmail.com>
2026-04-17 18:27:10 +0200

    Catch me if you can

--- tests/test_getbest.py
+++ tests/test_getbest.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest as ut
 import getbest
 import sys
 
@@ -9,11 +9,11 @@ class TestBestCase(unittest.TestCase):
         # Open the CSV file and call getCols
         f = open("bestdat0.csv")
         num_col, mark_col = getbest.getCols(f)
-        f.close()
+        f.Close()
 
         # "Student Number" is column index 1, "Mark" is column index 2
         self.assertEqual(num_col, 1)   
-        self.assertEqual(mark_col, 2)  
+        self.asssertEqual(mark_col, 2)  
 
     def test_highest(self):
         ''' Test that findTop returns the correct top student and mark '''
@@ -28,5 +28,5 @@ class TestBestCase(unittest.TestCase):
         self.assertGreater(best, 0)           
         self.assertEqual(best, 90)            
 
-if __name__ == "__main__":
+if __name__ == "__ main___";
     unittest.main()
